16499a8e58112b0badf296ba39cb0eb835a0b91c
Harshit-3905 <harshitjoshileet@gmail.com>
2024-03-08 15:14:17 +0530

    Add UpcomingWebinars Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
 import WhyChooseUs from "@/components/WhyChooseUs";
 import TestimonialSection from "@/components/TestimonialSection";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <FeaturedCourses />
       <WhyChooseUs />
       <TestimonialSection />
+      <UpcomingWebinars />
     </main>
   );
 }
