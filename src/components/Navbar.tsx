c6bced5f3f427a47d12889fc9974f8d429f54ed7
Harshit-3905 <harshitjoshileet@gmail.com>
2024-03-08 14:28:20 +0530

    Refactor Navbar component and fix links

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,22 +1,18 @@
 "use client";
+
 import React, { useState } from "react";
-import {
-  HoveredLink,
-  Menu,
-  MenuItem,
-  ProductItem,
-} from "@/components/ui/navbar-menu";
+import { HoveredLink, Menu, MenuItem, ProductItem } from "./ui/navbar-menu";
 import { cn } from "@/utils/cn";
 import Link from "next/link";
 
-const Navbar = ({ className }: { className?: string }) => {
+function Navbar({ className }: { className?: string }) {
   const [active, setActive] = useState<string | null>(null);
   return (
     <div
       className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}
     >
       <Menu setActive={setActive}>
-        <Link href="/">
+        <Link href={"/"}>
           <MenuItem
             setActive={setActive}
             active={active}
@@ -26,13 +22,13 @@ const Navbar = ({ className }: { className?: string }) => {
         <MenuItem setActive={setActive} active={active} item="Our Courses">
           <div className="flex flex-col space-y-4 text-sm">
             <HoveredLink href="/courses">All Courses</HoveredLink>
-            <HoveredLink href="/">Basic Music Theory</HoveredLink>
-            <HoveredLink href="/team">Advanced Composition</HoveredLink>
-            <HoveredLink href="/enterprise">Song Writing</HoveredLink>
-            <HoveredLink href="/enterprise">Music Production</HoveredLink>
+            <HoveredLink href="/courses">Basic Music Theory</HoveredLink>
+            <HoveredLink href="/courses">Advanced Composition</HoveredLink>
+            <HoveredLink href="/courses">Songwriting</HoveredLink>
+            <HoveredLink href="/courses">Music Production</HoveredLink>
           </div>
         </MenuItem>
-        <Link href="/">
+        <Link href={"/contact"}>
           <MenuItem
             setActive={setActive}
             active={active}
@@ -42,6 +38,6 @@ const Navbar = ({ className }: { className?: string }) => {
       </Menu>
     </div>
   );
-};
+}
 
 export default Navbar;
